c5158cfca09704928736ca73882d87bd65c2f9cc
'xxy' <'183951694@qq.com'>
2019-05-09 16:15:04 +0800

    表单添加

--- src/main/java/wms/app/bill/BillController.java
+++ src/main/java/wms/app/bill/BillController.java
@@ -10,6 +10,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,5 +44,28 @@ public class BillController {
         mp.put("count",count/pagesize+(count%pagesize==0?0:1));
         return freemarkerUtil.getString("bill.html", mp);
     }
-	
+	@GetMapping("/add")
+    public Object add(){
+        Map<String,Object> mp = new HashMap<String,Object>();
+        return freemarkerUtil.getString("/user/add.html", mp);
+    }
+	@PostMapping("/add")
+    public Object addin(
+    		@PathParam("plicyholder")String plicyholder,
+    		@PathParam("insured")String insured, 
+    		@PathParam("beneficiary")String beneficiary,
+    		@PathParam("effectivedate")String effectivedate,
+    		@PathParam("waitingperiod")String waitingperiod,
+    		@PathParam("amount")String amount,
+    		@PathParam("guaranteeperiod")String guaranteeperiod,
+    		@PathParam("paymentperiod")String paymentperiod,
+    		@PathParam("paymentday")String paymentday,
+    		@PathParam("bank")String bank,
+    		@PathParam("safeguard")String safeguard,
+    		@PathParam("line")String line,
+    		@PathParam("situation")String situation,
+    		@PathParam("mingya")String mingya){ 
+        Map<String,Object> mp = new HashMap<String,Object>();
+        return freemarkerUtil.getString("/user/add.html", mp);
+    }
 }
--- src/main/java/wms/app/bill/BillMapper.java
+++ src/main/java/wms/app/bill/BillMapper.java
@@ -2,6 +2,9 @@ package wms.app.bill;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -14,4 +17,36 @@ public interface BillMapper{
 	List<Bill> page(@Param("start") int start,@Param("length") int length);
 	@Select("SELECT COUNT(*) FROM sys_bill")
 	int count();
+	@Insert("INSERT INTO sys_bill("
+			+ "plicyholder,"
+			+ "insured,"
+			+ "beneficiary,"
+			+ "effectivedate,"
+			+ "waitingperiod,"
+			+ "amount,"
+			+ "guaranteeperiod,"
+			+ "paymentperiod,"
+			+ "paymentday,"
+			+ "bank,"
+			+ "safeguard,"
+			+ "line,"
+			+ "situation,"
+			+ "mingya"
+			+ ")VALUE("
+			+ "#{bill.plicyholder},"
+			+ "#{bill.insured},"
+			+ "#{bill.beneficiary},"
+			+ "#{bill.effectivedate},"
+			+ "#{bill.waitingperiod},"
+			+ "#{bill.amount},"
+			+ "#{bill.guaranteeperiod},"
+			+ "#{bill.paymentperiod},"
+			+ "#{bill.paymentday},"
+			+ "#{bill.bank},"
+			+ "#{bill.safeguard},"
+			+ "#{bill.line},"
+			+ "#{bill.situation},"
+			+ "#{bill.mingya}"
+			+ ")")
+	Bill save(Bill bill);
 }
